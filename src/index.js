dde87df36b6593660a8829b5b1362120fda4959a
emarf <frame.toravich@gmail.com>
2020-09-30 14:46:44 +0300

    feat: implement typical array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return (Array.isArray(array) && array.length > 1) ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return (Array.isArray(array) && array.length > 1) ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return (Array.isArray(array) && array.length > 1) ? array.reduce((sum, current) => sum + current, 0) / array.length : 0;
 }
